7e12042401e480357277c99eff4e6a01ee0829bc
fatima0203 <fatoumatabakm@gmail.com>
2024-12-31 23:23:41 +0100

    Add files via upload

---
+++ common.h
@@ -0,0 +1,20 @@
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+    typedef struct
+    {
+        int compteur;
+
+    } Message;
+
+
+
+#endif
